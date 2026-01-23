b5f6f058c70fe6a48351b2ef5b4e7be35455feaa
Subhransu De <subhransu.de.official@gmail.com>
2026-01-24 00:22:03 +0530

    Add types

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,12 +1,14 @@
 import json
 import logging
 import sys
-from logging import Formatter, Logger
+from logging import Formatter, Handler, Logger
+
+from app.settings import settings
 
 
 class JsonFormatter(Formatter):
     def format(self, record):
-        log_object = {
+        log_object: dict = {
             "timestamp": self.formatTime(record, self.datefmt),
             "name": record.name,
             "level": record.levelname,
@@ -19,11 +21,11 @@ class JsonFormatter(Formatter):
 
 formatter: Formatter = JsonFormatter()
 
-logger: Logger = logging.getLogger("fastapi_app")
-logger.setLevel(logging.DEBUG)
+logger: Logger = logging.getLogger(settings.app_name)
+logger.setLevel(logging.INFO)
 
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.DEBUG)
+console_handler: Handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 
 if not logger.handlers:
@@ -33,10 +35,10 @@ if not logger.handlers:
 def setup_logging():
     loggers = ["uvicorn", "uvicorn.access", "uvicorn.error"]
     for logger_name in loggers:
-        log = logging.getLogger(logger_name)
+        log: Logger = logging.getLogger(logger_name)
         log.handlers = []
 
-        handler = logging.StreamHandler(sys.stdout)
+        handler: Handler = logging.StreamHandler(sys.stdout)
         handler.setFormatter(formatter)
         log.addHandler(handler)
 
@@ -45,13 +47,13 @@ def setup_logging():
         log.propagate = False
 
 
-def info(msg: str, *args, **kwargs):
+def info(msg: str, *args, **kwargs) -> None:
     logger.info(msg, *args, **kwargs)
 
 
-def error(msg: str, *args, **kwargs):
+def error(msg: str, *args, **kwargs) -> None:
     logger.error(msg, *args, **kwargs)
 
 
-def debug(msg: str, *args, **kwargs):
+def debug(msg: str, *args, **kwargs) -> None:
     logger.debug(msg, *args, **kwargs)
